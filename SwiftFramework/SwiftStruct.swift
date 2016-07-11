016d714f3372d5fc392574ebbe091d54eb8bd206
Ben Chatelain <ben@octop.ad>
2016-07-10 22:46:25 -0600

    Add SwiftStruct

---
+++ SwiftFramework/SwiftStruct.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftStruct.swift
+//  SwiftFramework
+//
+//  Created by Ben Chatelain on 7/10/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+struct SwiftStruct {
+    let flag = true
+}
