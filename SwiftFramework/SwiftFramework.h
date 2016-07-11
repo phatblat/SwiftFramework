3d571a3c493bae02cc30d249e5b83c83882ab67a
Ben Chatelain <ben@octop.ad>
2016-07-10 22:41:31 -0600

    Add iOS framework target

---
+++ SwiftFramework/SwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFramework.h
+//  SwiftFramework
+//
+//  Created by Ben Chatelain on 7/10/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFramework.
+FOUNDATION_EXPORT double SwiftFrameworkVersionNumber;
+
+//! Project version string for SwiftFramework.
+FOUNDATION_EXPORT const unsigned char SwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFramework/PublicHeader.h>
+
+
